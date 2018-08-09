54eb85306547530279a0e7223d182f2ae2d44af7
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-09 18:26:24 -0500

    stuck on axios transmit FormData, leaving for now

--- client/components/game-forms/PictureSubmitForm.js
+++ client/components/game-forms/PictureSubmitForm.js
@@ -1,8 +1,7 @@
 import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
-
-export class PictureSubmitForm extends Component {
+export default class PictureSubmitForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -13,32 +12,50 @@ export class PictureSubmitForm extends Component {
 
   handleImageUpload = async evt => {
     evt.preventDefault()
-    try {
-      const data = new FormData()
-      data.append('file', this.uploadInput.files[0])
-      data.append('filename', this.fileName.value)
-      const submission = await axios.post('/picSubmit', data)
-      this.setState({submitted: true})
-    } catch (err) {
-      console.log('')
-    }
+    let data = new FormData()
+    data.append('image', this.uploadInput.files[0])
+    const image = data.getAll('image')
+    console.log('Tester', image)
+    const submission = await axios.post(
+      '/api/profileSubmissions/picSubmit',
+      {image},
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+
+    // data.append('filename', this.fileName.value)
+    // try {
+    //   data.append('file', this.uploadInput.files[0])
+    //   data.append('filename', this.fileName.value)
+    //   const submission = await axios.post('/picSubmit', data)
+    //   this.setState({submitted: true})
+    // } catch (err) {
+    //   console.log('')
+    // }
   }
   handleSubmit = evt => {
     evt.default()
     console.log('Submitted')
   }
   render() {
+    console.log('Pffft')
     return (
-      <Fragment>
+      <div>
         <form onSubmit={this.handleImageUpload}>
+          <label htmlFor="playerName"> PlayerName </label>
           <label htmlFor="imageUrl"> Player Picture </label>
           <input
             ref={ref => {
               this.uploadInput = ref
             }}
             type="file"
+            accept="image/*"
             name="imageUrl"
           />
+          <button type="submit"> Go </button>
         </form>
         {this.state.submitted ? (
           <Fragment>
@@ -46,7 +63,7 @@ export class PictureSubmitForm extends Component {
             <button> Continue </button>
           </Fragment>
         ) : null}
-      </Fragment>
+      </div>
     )
   }
 }
