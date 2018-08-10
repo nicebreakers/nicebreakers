a35e27628b7a92ec9e92b09eb639f95ad4225d04
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-10 17:42:01 -0500

    Created FourOFour and PreGame Hold

---
+++ client/components/in-game/index.js
@@ -0,0 +1,1 @@
+export {default as PreGameWaitPage} from './PreGameWait'
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, FourOFour} from './components'
 import {me} from './store'
 
 /**
@@ -24,11 +24,15 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route exact path="/home" component={UserHome} />
+            <Route exact path="/" component={UserHome} />
+            {/* <Route path="/createProfile" component={PictureSubmit} */}
+            <Route path="*" component={FourOFour} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        {/* <Route path="/" component={Login} /> */}
+        {/* <Route path="*" component={FourOFour} /> */}
       </Switch>
     )
   }
