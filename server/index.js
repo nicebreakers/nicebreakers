51d7a32800583cc6e2bf697a7f9e7dad5f4c434a
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-08-10 14:47:01 -0500

    fixed logout issue

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ passport.serializeUser((user, done) => done(null, user.id))
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.models.user.findById(id)
+    const user = await db.models.User.findById(id)
     done(null, user)
   } catch (err) {
     done(err)
