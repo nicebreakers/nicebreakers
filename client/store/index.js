de67b6296ba3b15813ce0c09bd3484a7899749f4
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-09 19:52:58 -0500

    Created Create Game Form

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import participants from './participantsReducer'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, participants})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
