96270cf6ca0779e0af449b6d84ac737ffec93b71
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-08-13 11:37:46 -0500

    removed row, col

--- client/components/controllers/PromptPhase.js
+++ client/components/controllers/PromptPhase.js
@@ -3,16 +3,12 @@ import React from 'react'
 const PromptPhase = props => {
   const {shape, prompt} = props
   return (
-    <div className="row">
-      <div className="col s12 m7">
-        <div className="card">
-          <div className="card-image">
-            <img src={shape} />
-          </div>
-          <div className="card-content">
-            <p>{prompt}</p>
-          </div>
-        </div>
+    <div className="card">
+      <div className="card-image">
+        <img src={shape} />
+      </div>
+      <div className="card-content">
+        <p>{prompt}</p>
       </div>
     </div>
   )
