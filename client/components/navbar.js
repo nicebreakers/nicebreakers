557bc0d10ccc77c696e21b4fc4e5ef819a62d048
Patrick Kilgore <pckilgore@noreply.github.com>
2018-08-13 10:12:09 -0500

    Navbar!

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,33 +4,55 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+// TODO -- Use an AUTH higher order component.
+
+const LoggedInLinks = props => (
+  <span>
+    <li>
+      <Link to="/home">Home</Link>
+    </li>
+    <li>
+      <a href="#" onClick={props.handleClick}>
+        Logout
+      </a>
+    </li>
+  </span>
+)
+
+const LoggedOutLinks = props => (
+  <span>
+    {' '}
+    <li>
+      {' '}
+      <Link to="/login">Login</Link>
+    </li>
+    <li>
+      <Link to="/signup">Sign Up</Link>
+    </li>
+  </span>
+)
+const Navbar = props => (
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+    <nav className="light-blue lighten-3" role="navigation">
+      <div className="nav-wrapper container">
+        <Link to="/" className="brand-logo logo-container">
+          <i className="material-icons">ac_unit</i>
+          <span className="flow-text truncate"> Nicebreakers </span>
+        </Link>
+        <ul className="right hide-on-med-and-down">
+          {props.isLoggedIn ? <LoggedInLinks {...props} /> : <LoggedOutLinks />}
+        </ul>
+        <ul id="nav-mobile" className="sidenav">
+          {props.isLoggedIn ? <LoggedInLinks {...props} /> : <LoggedOutLinks />}
+        </ul>
+        <a href="#" data-target="nav-mobile" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
     </nav>
-    <hr />
   </div>
 )
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
