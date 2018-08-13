409f8f6c5a7c9ef86c90536bfc72b26c060281d7
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-08-13 11:35:14 -0500

    removing row

--- client/components/controllers/GameEnded.js
+++ client/components/controllers/GameEnded.js
@@ -3,8 +3,8 @@ import React from 'react'
 const GameEnded = props => {
   return (
     <div className="container">
-      <div className="row center">
-        <h5 className="card-panel blue lighten-4 s12">Thank you for playing</h5>
+      <div className="center">
+        <h5 className="card-panel blue lighten-4">Thank you for playing</h5>
       </div>
       {/* link for end of game summary */}
       <button type="button">Go to Dashboard</button>
