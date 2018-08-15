2e0e13d0db7b15bdac218bf16a6ecb9fb0e426ce
Patrick Kilgore <pckilgore@noreply.github.com>
2018-08-14 21:01:50 -0500

    Additional fixes  before we all dig into sockets

--- client/components/user-home/cardAdminTools.js
+++ client/components/user-home/cardAdminTools.js
@@ -5,12 +5,16 @@ const CardAdminTools = ({id, type}) => {
   let actionName = ''
   if (type === 'done') actionName = 'remove'
   if (type === 'pending') actionName = 'edit'
-
   return (
     <div className="card-action right-align">
       <Link to={`/events/${id}/${actionName}`} className="deep-orange-text">
         {actionName}
       </Link>
+      {type === 'pending' && (
+        <Link to={`/events/${id}/console`} className="teal-text">
+          Start Event
+        </Link>
+      )}
     </div>
   )
 }
--- client/components/user-home/eventCard.js
+++ client/components/user-home/eventCard.js
@@ -18,7 +18,7 @@ const style = {
 const EventCard = ({type, title, details, id}) => (
   <div className="col s12 m6 l4 xl3">
     <div className={`card hoverable ${style[type].background}`}>
-      <Link to="#">
+      <Link to={`/events/${id}/controller`}>
         <div className={`card-content ${style[type].contentText}`}>
           <span className={`card-title ${style[type].titleText}`}>
             {title ? title : 'Placeholder title'}
