a554e7fc5b75de6f8eb1b2b09b4704ffcaa187c6
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-15 09:47:15 -0500

    Updated thunk to pull event ID

--- client/store/event.js
+++ client/store/event.js
@@ -4,7 +4,6 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_EVENTS = 'GET_EVENTS'
-const GET_EVENT = 'GET_EVENT'
 
 const ADD_EVENT = 'ADD_EVENT'
 const UPDATE_EVENT_ALL = 'UPDATE_EVENT_ALL'
@@ -44,12 +43,9 @@ export const fetchAllEvents = () => async dispatch => {
   dispatch(getEvents(events))
 }
 //Put Request for all fields on event
-export const changeEventAllFields = (
-  eventSubmission,
-  eventId
-) => async dispatch => {
+export const changeEventAllFields = eventSubmission => async dispatch => {
   const {data: updatedEvent} = await axios.put(
-    `/api/events/${eventId}`,
+    `/api/events/${eventSubmission}`,
     eventSubmission
   )
   dispatch(updateEventAll(updatedEvent))
