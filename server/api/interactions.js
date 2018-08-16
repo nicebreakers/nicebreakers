68928571382a361119e77cf109c75efcf696d20f
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-15 19:09:24 -0500

    Put route for Interaction Input

---
+++ server/api/interactions.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const {Interaction} = require('../db/models')
+const canOnlyBeUsedBy = require('./authMiddleware')
+module.exports = router
+
+//Put Route for Interaction Input. TODO: Account for User Input Score
+router.put(
+  '/:interactionId',
+  canOnlyBeUsedBy('admin', 'participant', 'self'),
+  async (req, res, next) => {
+    try {
+      const {round, userInput} = req.body
+
+      const interaction = await Interaction.findById(req.params.interactionId)
+      const {aId, bId} = interaction
+
+      //Check the Round?
+      if (round !== interaction.round)
+        throw 'Round does not match the interaction round'
+
+      //Check to see if the user is either a or b
+      if (req.user.id === aId) interaction.update({aInput: userInput})
+      else if (req.user.id === bId) interaction.update({bInput: userInput})
+      res.json(interaction)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
