7c02453e9df636699c0dda1abc700f8a1cd1deee
Brendan Meyer <brenmeyer2245@gmail.com>
2018-08-16 18:31:55 -0500

    Moved Event Controls to Single Event Page

--- client/components/player-single.js
+++ client/components/player-single.js
@@ -3,8 +3,8 @@ import React from 'react'
 const PlayerSingle = props => {
   const {data, handleClick} = props
   return (
-    <div>
-      <h3 style={{display: 'inline-block'}}>{data}</h3>
+    <div className="col s6">
+      <h5 style={{display: 'inline-block'}}>{data}</h5>
       <button type="button" onClick={event => handleClick(event, data)}>
         Remove
       </button>
--- client/components/single-event.js
+++ client/components/single-event.js
@@ -1,52 +1,70 @@
 import React from 'react'
 import PlayerList from './player-list'
 import PlayerAdd from './player-add'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-const mockData = {
-  location: 'Chicago',
-  name: 'A Test Event',
-  date: '2018',
-  description: 'A Test Event that Tests',
-  status: 'Pending',
-  Players: [{name: 'John'}, {name: 'Jack'}, {name: 'Jill'}]
+const SingleEventPage = props => {
+  return props.event ? (
+    <div className="container">
+      <div className="row">
+        <div className="col s12 m6">
+          <label> Event Name</label>
+          <p> {props.event.name}</p>
+        </div>
+        <div className="col s12 m6">
+          <label> Date of Event </label>
+          <p> {props.event.date}</p>
+        </div>
+        <div className="col s12 m6 ">
+          <label> Location</label>
+          <p> {props.event.location}</p>
+        </div>
+        <div className="col s12 m6">
+          <label> Event Status </label>
+          <p> {props.event.status}</p>
+        </div>
+        <div className="col">
+          <label> Description</label>
+          <p> {props.event.description} </p>
+        </div>
+      </div>
+      <Link to={`/events/${props.event.id}/edit`} className="white-text btn">
+        Edit Event
+      </Link>
+      <div className="row">
+        <PlayerList />
+      </div>
+      <div className="row">
+        <div className="col s6">
+          <p> Add Some Friends </p>
+          <PlayerAdd />
+        </div>
+        <div className="col s6">
+          <button
+            className="btn"
+            button="button"
+            onClick={() => console.log('Invites Sent')}
+          >
+            {' '}
+            Send out Invites!{' '}
+          </button>
+        </div>
+      </div>
+      {props.event.status === 'pending' && (
+        <div className="btn m7">
+          <Link to={`/events/${props.event.id}/console`} className="white-text">
+            Go Event Controls
+          </Link>
+        </div>
+      )}
+    </div>
+  ) : (
+    <h1> Loading </h1>
+  )
 }
+const mapState = (state, {match}) => ({
+  event: state.events.byId[match.params.eventId]
+})
 
-const SingleEventPage = props => (
-  <div className="Container">
-    <div className="row">
-      <div className="col s12 m6">
-        <label> Event Name</label>
-        <p> {mockData.name}</p>
-      </div>
-      <div className="col s12 m6">
-        <label> Date of Event </label>
-        <p> {mockData.date}</p>
-      </div>
-    </div>
-    <div className="col s12 m6">
-      <label> Location</label>
-      <p> {mockData.location}</p>
-    </div>
-    <div className="col s12 m6">
-      <label> Event Status </label>
-      <p> {mockData.status}</p>
-    </div>
-    <div>
-      <label> Description</label>
-      <p> {mockData.description} </p>
-    </div>
-    <div>
-      <PlayerList />
-      <p> Add Some Friends </p>
-      <PlayerAdd />
-    </div>
-    <div>
-      <button button="button" onClick={() => console.log('Invites Sent')}>
-        {' '}
-        Send out Invites!{' '}
-      </button>
-    </div>
-  </div>
-)
-
-export default SingleEventPage
+export default connect(mapState)(SingleEventPage)
