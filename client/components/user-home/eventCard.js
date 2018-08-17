88b8b9e7bac5a65aac4313e273758b00eca97632
Patrick Kilgore <pckilgore@noreply.github.com>
2018-08-17 11:17:13 -0500

    style tweak to cards

--- client/components/user-home/eventCard.js
+++ client/components/user-home/eventCard.js
@@ -21,7 +21,7 @@ const style = {
 
 const EventCard = ({type, title, details, id}) => (
   <div className="col s12 m6 l4 xl3">
-    <div className={`card hoverable medium ${style[type].background}`}>
+    <div className={`card hoverable small ${style[type].background}`}>
       <Link to={`/events/${id}/`}>
         <div className={`card-content ${style[type].contentText}`}>
           <span className={`card-title ${style[type].titleText}`}>{title}</span>
