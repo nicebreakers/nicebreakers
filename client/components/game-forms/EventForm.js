f5624398d6f3c7b1b015d73ec067da2b6f0df2f1
Brendan Meyer <31311247+brenmeyer2245@users.noreply.github.com>
2018-08-18 13:45:04 -0500

    Event Form Validations for Name and Event Date (#210)

--- client/components/game-forms/EventForm.js
+++ client/components/game-forms/EventForm.js
@@ -2,22 +2,57 @@ import React, {Fragment} from 'react'
 import {Field, reduxForm} from 'redux-form'
 import propTypes from 'prop-types'
 
+/*
+* Custom Validations
+*/
+const futureDate = dateValue => {
+  return new Date(dateValue) < Date.now()
+    ? 'Event must be in the future'
+    : undefined
+}
+const nameHasValue = name => {
+  return !name || name === '' ? 'Event must have a name' : undefined
+}
+
+/**
+ *
+ * Component for rendering any form fields that need to validate
+ */
+const renderField = ({input, type, label, meta: {touched, error}}) => (
+  <div>
+    <input {...input} placeholder={label} type={type} />
+    {touched &&
+      (error && (
+        <div className="btn red">
+          <span> {error} </span>
+        </div>
+      ))}
+  </div>
+)
+
 let EventForm = props => {
   const {handleSubmit, pristine, submitting} = props
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="eventName">Event Name </label>
-      <Field name="name" component="input" type="text" placeholder="My Event" />
+      <Field
+        name="name"
+        component={renderField}
+        type="text"
+        placeholder="My Event"
+        validate={nameHasValue}
+      />
       <label htmlFor="date">Date of Event</label>
       <Field
         name="date"
-        component="input"
+        component={renderField}
         type="date"
-        placeholder={Date.now()}
+        label={Date.now()}
+        validate={futureDate}
+        warn={futureDate}
       />
       <label htmlFor="location"> Event Location</label>
-
       <Field
         name="location"
         component="input"
@@ -45,6 +80,7 @@ let EventForm = props => {
           </Field>
         </Fragment>
       )}
+
       <button
         className="btn waves waves-light"
         type="submit"
