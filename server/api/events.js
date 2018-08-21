b81d35e2fe4ed68b74174760bcd5bec7654c566d
Brendan Meyer <31311247+brenmeyer2245@users.noreply.github.com>
2018-08-21 09:19:05 -0500

    Removed Event Creator as default participant (#239)

--- server/api/events.js
+++ server/api/events.js
@@ -45,8 +45,6 @@ router
         location
       })
       //add user to the event
-      const currentUser = await User.findById(req.user.id)
-      newEvent.addUser(currentUser)
       res.send(newEvent)
     } catch (err) {
       next(err)
